5008dd1763f7e8219bac36a562bdc282cbe94200
unknown <seiautomacao.matheus@gmail.com>
2025-07-12 18:20:55 -0300

    alteracao de algumas paginas

--- firebase.js
+++ firebase.js
@@ -5,11 +5,11 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDC1qtoweF5dlV_nx1PSLCK291Pv9KNGkg",
-  authDomain: "matafome-9413b.firebaseapp.com",
+  authDomain: "pedeai-9413b.firebaseapp.com",
   // databaseURL não é estritamente necessário para Firestore, mas não atrapalha
-  databaseURL: "https://matafome-9413b-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "matafome-9413b",
-  storageBucket: "matafome-9413b.firebasestorage.app",
+  databaseURL: "https://pedeai-9413b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pedeai-9413b",
+  storageBucket: "pedeai-9413b.firebasestorage.app",
   messagingSenderId: "315880064175",
   appId: "1:315880064175:web:22f3c0b9714b7b1ca5c05a",
   measurementId: "G-SKXD29NM1J"
