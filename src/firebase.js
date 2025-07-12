ee8bc14ed56599ef89e50a5bb11a882d13110fb6
unknown <seiautomacao.matheus@gmail.com>
2025-07-12 19:20:24 -0300

    Atualiza credenciai do firebase

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,15 @@ import { getFirestore } from 'firebase/firestore'; // Para o Firestore Database
 
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDC1qtoweF5dlV_nx1PSLCK291Pv9KNGkg',
-  authDomain: 'matafome-9413b.firebaseapp.com',
-  databaseURL: 'https://matafome-9413b-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'matafome-9413b',
-  storageBucket: 'matafome-9413b.appspot.com',
-  messagingSenderId: '315880064175',
-  appId: '1:315880064175:web:22f3c0b9714b7b1ca5c05a'
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
+
 // Inicialize o aplicativo Firebase
 const app = initializeApp(firebaseConfig);
 
