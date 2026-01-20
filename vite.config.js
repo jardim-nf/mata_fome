fcc016abe71b9f0a5726c93c97535315711cb6a6
jardim_nf <seiautomacao.matheus@gmail.com>
2026-01-19 21:27:20 -0300

    atualizacao

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,18 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
+    // ADICIONE ESTE BLOCO 'server' para permitir acesso na rede local (opcional, ajuda a testar no celular)
+    server: {
+      host: true
+    },
+    // ADICIONE ESTE BLOCO 'build'
+    build: {
+      // 'es2015' garante compatibilidade com iOS mais antigos
+      target: 'es2015', 
+      // Opcional: minificação correta para evitar bugs no Safari
+      cssMinify: true,
+      sourcemap: false,
+    },
     define: {
       // Expõe as variáveis de ambiente para a sua aplicação
       'process.env': env
