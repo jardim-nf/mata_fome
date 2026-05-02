8b3487bf60a74f9deea3d6dc985a6c5bee083c82
jardim_nf <seiautomacao.matheus@gmail.com>
2026-05-02 17:58:06 -0300

    fix(karaoke): corrige caminho do import do firebase no KaraokeTV

--- src/pages/KaraokeTV.jsx
+++ src/pages/KaraokeTV.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from '../../firebase';
+import { db } from '../firebase';
 import { collection, onSnapshot, query, where } from 'firebase/firestore';
 import { FaMicrophoneAlt, FaMusic } from 'react-icons/fa';
 
